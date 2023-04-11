a587b2cb7bd020f916dcc2d3948e2e0d7436d404
jacke Vera <jackesjavemu@gmail.com>
2023-04-10 20:59:23 -0500

    ultimo

---
+++ root/scripts/main.js
@@ -0,0 +1,13 @@
+const userName = window.prompt("what is your name");
+
+console.log (userName);
+if (username ==="admin"){
+    const userName = window.prompt("what is your password");
+    if (usepassword === "themaster"){
+        console.log ("bienvenido");
+    } else {
+    console.log ("no te conozco");
+} else {
+    cost userPassword = window.prompt("what is your password?");
+    
+}
